465b44d37bd5e861be36dcfc37b41b06891e918c
manasavijay09 <manasa.vijay09@gmail.com>
2024-02-14 20:06:26 -0600

    Initila models with user & address.js files

---
+++ models/Addresses.js
@@ -0,0 +1,69 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create Project model and datatypes, including the user_id foreign key.
+class Addresses extends Model {}
+//name(optional)
+//change to singler--add
+Addresses.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    address_line1: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    address_line2: {
+      type: DataTypes.STRING(100),
+      allowNull:true,
+    
+    },
+    city: {
+      type: DataTypes.STRING(25),
+      allowNull: false,
+      validate: {
+        isAlpha: true, 
+      },
+    },
+    state: {
+      type: DataTypes.STRING(2),
+      allowNull: false,
+      validate: {
+        isAlpha: true, 
+      },
+    },
+    zip: {
+      type: DataTypes.INTEGER(5),
+      allowNull: false,
+      validate: {
+        isNumeric: true, 
+      },
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'addresses',
+  }
+);
+
+module.exports = Addresses;
---
+++ models/User.js
@@ -0,0 +1,76 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class User extends Model {
+  // Checks the password against the encrypted password in the database.
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+//add date stamp as wll
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        msg: "You must enter Phone Number",
+        len: [10, 10],
+        isInt: true,
+      },
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+        isAlphanumeric: true,
+      },
+    },
+  },
+  {
+    // Hooks are used so that if a user is created or updated, the password is encrypted before being stored in the database.
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
